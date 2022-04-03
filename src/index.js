6f95776216f0eb9e5c82536d6cb2948c5b37f8d3
rofiqusmonov613 <rofiqusmonov613@gmail.com>
2022-04-03 16:20:14 +0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const {PORT, pool} = require('./connect.js')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/', async(req, res) => {
    const client = await pool.connect()
